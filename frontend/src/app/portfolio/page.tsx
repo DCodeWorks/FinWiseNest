ca4854b1df100bf550b3c0076fc80bb60ece4ca4
Daniel Hajek <danielhajek85@gmail.com>
2025-07-03 15:41:29 +0200

    frontend fetching data from .net api

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -1,9 +1,27 @@
 import { PortfolioView } from "@/components/PortfolioView";
-import { mockHoldings } from "../lib/portfolio/types";
+import { Holding } from "../lib/portfolio/types";
 
-export default function PortfolioPage() {
-  const holdings = mockHoldings;
+async function getPortfoliodata(): Promise<Holding[]> {
+  try {
+    const response = await fetch("http://localhost:5000/api/portfolio", {
+      cache: "no-store",
+    });
+    if (!response.ok) return [];
+
+    const data: Holding[] = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('An error occurred while fetching data:', error);
+    return [];
+  }
+}
+
+export default async function PortfolioPage() {
+  const holdings = await getPortfoliodata();
   const totalValue = holdings.reduce((acc, h) => acc + h.marketValue, 0);
+
+  if (holdings.length === 0) return <h1>No data available</h1>;
 
   return <PortfolioView holdings={holdings} portfolioValue={totalValue} />;
 }
